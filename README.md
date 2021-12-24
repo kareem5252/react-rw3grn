bd2fe882d296edbb4fd4316b9179afee1f6d3ca5
kareem5252 <hackercan5252@gmail.com>
2021-12-24 05:04:22 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-rw3grn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-rw3grn)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function App() {
+  return (
+    <div className="container bg-orange-800 p-3">
+      <h1 className="text-white text-center">Majeed Najmi</h1>
+    </div>
+  );
+}
+
+export default App;
